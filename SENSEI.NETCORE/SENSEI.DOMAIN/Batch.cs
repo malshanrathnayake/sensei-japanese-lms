2971f5c7aa776a53214a44a4e063c4fc8d4716a6
Malshan Rathnayake <malshan.edu@gmail.com>
2026-02-20 19:33:30 +0530

    sitemap and seo meta data

--- SENSEI.NETCORE/SENSEI.DOMAIN/Batch.cs
+++ SENSEI.NETCORE/SENSEI.DOMAIN/Batch.cs
@@ -1,21 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SENSEI.DOMAIN
 {
     public class Batch
     {
+        [DisplayName("Batch")]
         public long BatchId { get; set; }
+
         [Required]
+        [DisplayName("Course")]
         public long CourseId { get; set; }
+
         [Required]
+        [DisplayName("Batch Name")]
         public string BatchName { get; set; }
+
         [Required]
+        [DisplayName("Start Date")]
         public DateTime BatchStartDate { get; set; }
+
         [Required]
+        [DisplayName("End Date")]
         public DateTime BatchEndDate { get; set; }
+
         public bool IsDeleted { get; set; }
+
         public string EncryptedKey { get; set; }
 
         // Navigation
