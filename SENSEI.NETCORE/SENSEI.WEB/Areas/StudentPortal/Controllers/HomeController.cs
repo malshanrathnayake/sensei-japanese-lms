1ff04c6f952ead1731599b51066b24019e46d383
Santhushie Nallaperuma <santhushie@gmail.com>
2026-03-06 09:12:14 +0530

    ui update

--- SENSEI.NETCORE/SENSEI.WEB/Areas/StudentPortal/Controllers/HomeController.cs
+++ SENSEI.NETCORE/SENSEI.WEB/Areas/StudentPortal/Controllers/HomeController.cs
@@ -5,9 +5,25 @@ namespace SENSEI.WEB.Areas.StudentPortal.Controllers
     [Area("StudentPortal")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly SENSEI.BLL.StudentPortalService.Interfaces.IStudentService _studentService;
+
+        public HomeController(SENSEI.BLL.StudentPortalService.Interfaces.IStudentService studentService)
         {
-            var studentId = Convert.ToInt64(HttpContext.Session.GetString("StudentId"));
+            _studentService = studentService;
+        }
+
+        public async System.Threading.Tasks.Task<IActionResult> Index()
+        {
+            var studentIdStr = HttpContext.Session.GetString("StudentId");
+            if (string.IsNullOrEmpty(studentIdStr)) return Redirect("~/auth/login");
+
+            long studentId = Convert.ToInt64(studentIdStr);
+
+            var (courses, totalCourses) = await _studentService.SearchStudentCourses(studentId, 0, 10);
+            var (lessons, totalLessons) = await _studentService.SearchStudentBatchLessons(studentId, 0, 0, 5, "", "lessonDateTime", "DESC"); 
+
+            ViewBag.TotalCourses = totalCourses;
+            ViewBag.UpcomingLessons = lessons;
 
             return View();
         }
