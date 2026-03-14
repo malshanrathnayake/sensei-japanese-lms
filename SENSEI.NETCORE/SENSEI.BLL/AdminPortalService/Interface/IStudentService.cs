4a68d00fbee40614acfed6a58f814065db1f8b7c
Malshan Rathnayake <malshan.edu@gmail.com>
2026-03-14 18:51:36 +0530

    student list page

---
+++ SENSEI.NETCORE/SENSEI.BLL/AdminPortalService/Interface/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using SENSEI.DOMAIN;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SENSEI.BLL.AdminPortalService.Interface
+{
+    public interface IStudentService
+    {
+        Task<(IEnumerable<Student>, long)> SearchStudent(long courseId = 0, long batchId =0, int start = 0, int length = 10, string searchValue = "", string sortColumn = "", string sortDirection = "");
+    }
+}
